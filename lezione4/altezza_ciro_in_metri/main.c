ea64f45bb6cb39939ad0e3a62ee6fd937740393d
gx1 <gx1@secsi.io>
2022-04-29 18:28:58 +0200

    Altezza in metri di ciro

---
+++ lezione4/altezza_ciro_in_metri/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+
+    const float ALTEZZA_VERA_CIRO_METRI = 1.86;
+    float altezzaSceltaDaUtente;
+
+    printf("L'altezza vera in metri e' %f\n", ALTEZZA_VERA_CIRO_METRI);
+    printf("Che altezza pensi abbia?\n");
+    scanf("%f", &altezzaSceltaDaUtente);
+
+    printf("L'utente ha scelto %f\t", altezzaSceltaDaUtente);
+
+	// == -> VUOL DIRE UGUAGLIANZA
+	// != .> VUOL DIRE DISUGUAGLIANZA
+	if (altezzaSceltaDaUtente == ALTEZZA_VERA_CIRO_METRI)
+    {
+
+        printf("Hai indovinato altezza");
+    }
+    else
+    {
+        printf("Hai sbagliato, riprova");
+    }
+    return 0;
+}
