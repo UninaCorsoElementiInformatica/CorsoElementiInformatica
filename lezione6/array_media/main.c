bb51cdd397143237e08e6cd9d055561644152f1d
gx1 <gx1@secsi.io>
2022-05-06 18:00:17 +0200

    matrici

--- lezione6/array_media/main.c
+++ lezione6/array_media/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 int main()
 {
     int DIM =3;
---
+++ lezione6/matrici/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+
+    const int DIM_RIGA = 3;
+    const int DIM_COL = 2;
+    int vettore[3];
+    int matrice[DIM_RIGA][DIM_COL];
+    int i, j;
+    // Acquisizione valori
+    for (i = 0; i < DIM_RIGA; i++)
+        for(j = 0; j < DIM_COL; j++)
+        {
+            printf("Inserisci riga %d colonna %d\n", i, j);
+            scanf("%d", &matrice[i][j]);
+
+        }
+
+    // Stampa valori
+    for (i = 0; i < DIM_RIGA; i++)
+    {
+        for(j = 0; j < DIM_COL; j++)
+        {
+           printf("%d\t", matrice[i][j]);
+        }
+        printf("\n");
+    }
+
+
+
+    return 0;
+}
